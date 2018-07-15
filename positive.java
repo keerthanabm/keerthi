9896741c4b356c50a7e312137052b7c0c27641fa
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-15 10:40:30 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,20 @@
+package test4;
+
+public class Positive {
+	public static void main(String args[]){
+		
+		int n=Integer.parseInt(args[0]);
+		if(n==0){
+			System.out.print("zero");
+		}
+		else if(n>0){
+			System.out.print("positive");
+		}
+		else
+		{
+			System.out.print("negative");
+		}
+		}
+
+	}
+
