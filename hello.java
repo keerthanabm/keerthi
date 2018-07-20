4ae281f24e8b0244c905f52e4f4c914e62f7aced
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-20 10:07:58 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class hello {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String a=sc.next();
+		int i;
+		for(i=0;i<n;i++){
+			System.out.println(a);
+		}
+	}
+
+}
