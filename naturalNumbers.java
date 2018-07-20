7fff69647ce70140010f76db6acf88a75ab383cf
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-20 10:16:20 +0530

    Create naturalNumbers.java

---
+++ naturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class naturalNumbers {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i;
+		int count=0;
+		for(i=0;i<=n;i++){
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+
+}
