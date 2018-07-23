31c1a753278440621418dd45788279fc9811f3de
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-23 12:17:10 +0530

    Update intervelodd.java

--- intervelodd.java
+++ intervelodd.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 public class intervelodd {
 	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
 		int i;
-		for(i=2;i<6;i++){
+		for(i=n;i<m;i++){
 			if(i%2!=0){
 				System.out.println(i);
 			}
