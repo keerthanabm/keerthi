0daca1669fea2530e3c1d253399e999a7e86b21a
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-23 12:15:05 +0530

    Create intervelodd.java

---
+++ intervelodd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class intervelodd {
+	public static void main(String args[]){
+		int i;
+		for(i=2;i<6;i++){
+			if(i%2!=0){
+				System.out.println(i);
+			}
+		}
+	}
+
+}
