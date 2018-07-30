80bb7307ac9ed8134cfc8896d06e94a79b7c15fa
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-07-30 17:34:16 +0530

    Create loop2.java

---
+++ loop2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class loop2 {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int i;
+	
+	
+	
+	if(n>=1 && n<=10){
+		System.out.println("yes");
+	}
+
+	
+	else{
+		System.out.println("no");
+	}
+	
+}
+}
+
