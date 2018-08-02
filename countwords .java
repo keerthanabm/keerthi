9c8ebd5a7a33dca026994db41fdb4525f7493def
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-08-02 11:27:16 +0530

    Create  countwords .java

---
+++ countwords .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class countwords {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		String[] b=a.split(" ");
+		int i,count=0;
+		for(i=0;i<b.length;i++){
+			count++;
+		}
+		System.out.println(count);
+				
+	}
+
+}
