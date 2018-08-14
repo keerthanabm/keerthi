abe23d2a7d97c754735649d55971b41e276ece38
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-08-14 16:48:56 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class switch {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		
+		switch(a){
+		case 1:
+		System.out.println("one");
+           break;
+		case 2:
+			
+				System.out.println("Two");
+			break;
+		case 3:
+			
+				System.out.println("Three");
+				break;
+		case 4:
+			
+				System.out.println("four");
+				break;
+		case 5:
+			
+				System.out.println("five");
+				break;
+		case 6:
+			
+				System.out.println("six");
+				break;
+		case 7:
+			
+				System.out.println("Seven");
+				break;
+		case 8:
+			
+				System.out.println("eight");
+				break;
+		case 9:
+		
+				System.out.println("nine");
+				break;
+		case 10:
+			
+				System.out.println("ten");
+				break;
+		
+		default:
+			System.out.println("Invalid");
+			break;
+		
+		}	
+	}
+
+
+	}
+
