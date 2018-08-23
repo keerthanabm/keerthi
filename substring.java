5e4c8fa26edc9a92be452da942ac8aa7dbcd7d91
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-08-23 10:07:27 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class substring {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+	
+		if(a.contains(b))
+		
+		System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+
+}
