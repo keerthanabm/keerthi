c130e120b9332dc2ce189ce99cfa404b2105340f
keerthanabm <41110136+keerthanabm@users.noreply.github.com>
2018-09-19 20:46:31 +0530

    Create PrintFirstduplicate.java

---
+++ PrintFirstduplicate.java
@@ -0,0 +1,25 @@
+ import java.util.Scanner;
+  public class PrintFirstduplicate {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []a=new int[n];
+        int i,j,count=0;
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            count=0;
+            for(j=i+1;j<n;j++){
+                if((a[i]==a[j]) &&(i!=j)){
+                   }
+            
+               }
+        }
+         for(i=0;i<=0;i++){
+                       System.out.println(a[i]); 
+                    }
+                        
+        
+    }
+}
